68fa302f38d4c2ada89bf9db7650218b083df6c9
Antonino Lo Bianco <antonino.lobianco@outlook.com>
2023-12-22 08:07:04 +0100

    css fini cld mtn

--- routes/downloadsRoutes.js
+++ routes/downloadsRoutes.js
@@ -6,20 +6,20 @@ const cloudinary = require('../middleware/cloudinaryConfig');
 const router = Router();
 
 router.get('/upload', userController.downloadImage_get)
-router.post('/upload', upload.single('profile-file'), userController.downloadImage)
-// router.post('/upload', upload.single('profile-file'), async (req, res) => {
-//     try {
-//         console.log('Request received for image upload');
-//         const result = await cloudinary.uploader.upload(req.file.path);
-//         console.log(result);
-//         // Ici, tu peux utiliser result.secure_url comme nécessaire
-//         req.body.image = result.secure_url;
-//         userController.downloadImage(req, res);
-//     } catch (error) {
-//         console.error('Error in image upload route:', error);
-//         res.status(500).json({ success: false, error: 'Erreur lors de l\'upload de l\'image' });
-//     }
-// });
+// router.post('/upload', upload.single('profile-file'), userController.downloadImage)
+router.post('/upload', upload.single('profile-file'), async (req, res) => {
+    try {
+        console.log('Request received for image upload');
+        const result = await cloudinary.uploader.upload(req.file.path);
+        console.log(result);
+        // Ici, tu peux utiliser result.secure_url comme nécessaire
+        req.body.image = result.secure_url;
+        userController.downloadImage(req, res);
+    } catch (error) {
+        console.error('Error in image upload route:', error);
+        res.status(500).json({ success: false, error: 'Erreur lors de l\'upload de l\'image' });
+    }
+});
 
 
 module.exports = router;
